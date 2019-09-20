08988af5a74517b206cd2f12b76750804627a1fb
alexalrobinson <31484232+alexalrobinson@users.noreply.github.com>
2019-09-20 12:26:50 -0400

    Update api.php

--- api.php
+++ api.php
@@ -8,7 +8,7 @@
     //the limit 0, 10 takes the first 10 results.
     // you might want to consider taking more results, implementing "pagination", 
     // ordering by rank, etc.
-    $query = "SELECT rack, words FROM racks WHERE rack IN (SELECT rack FROM racks WHERE length>7 order by random() limit 0, 1)";
+    $query = "SELECT rack FROM racks WHERE length>7 order by random() limit 0, 1";
     
     //this next line could actually be used to provide user_given input to theo 
     //avoid SQL injection attacks
@@ -19,6 +19,8 @@
     //there are several ways of getting the data out, iterating row by row,
     //I chose to get associative arrays inside of a big array
     //this will naturally create a pleasant array of JSON data when I echo in a couple lines
+    $twisted = $statement->fetchSingle(PDO::FETCH_ASSOC);
+
     $results = $statement->fetchAll(PDO::FETCH_ASSOC);
     
     //this part is perhaps overkill but I wanted to set the HTTP headers and status code
