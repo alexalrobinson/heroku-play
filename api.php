ba3ab9be8c4d0a302ad5612eaf153d07e755cc2b
alexalrobinson <31484232+alexalrobinson@users.noreply.github.com>
2019-09-22 14:17:42 -0400

    Update api.php

--- api.php
+++ api.php
@@ -1,36 +1,26 @@
 <?php
-$dbhandle = new PDO("sqlite:scrabble.sqlite") or die("Failed to open DB");
-    if (!$dbhandle) die ($error);
-	if($_GET["button"] == "newGame")
-	{
-        $query = "SELECT rack, words FROM racks WHERE length=7 and weight <= 10 order by random() limit 0, 1";
-    
-        $statement = $dbhandle->prepare($query);
-        $statement->execute();
-        
-        $results = $statement->fetch(PDO::FETCH_ASSOC);
-        echo json_encode($results);
-	//$words_arr = explode("@@", $results["words"]);
-	//echo json_encode($words_arr);
-    //$words_arr = explode("@@", $results["words"]);
-	}
-	else if($_GET["button"] == "check")
-	{
-	    $query = "select * from racks where rack = '" . $_GET["rack"]."'";
-	    $statement = $dbhandle->prepare($query);
-        $statement->execute();
-        $row = $statement->fetch(PDO::FETCH_ASSOC);
-        $word_arr = explode("@@", $row["words"]);
-        $arr1 = array("result" => "incorrect");
-        $arr2 = array("result" => "correct" , "guess"=>$_GET["guess"]);
-        if(in_array($_GET["guess"], $word_arr))
-        {
-            //if correct
-            echo json_encode($arr2);
-        } 
-        else
-        {
-            echo json_encode($arr1);
-        }
-	}
+  $dbhandle = new PDO("sqlite:scrabble.sqlite") or die("Failed to open DB");
+  if (!$dbhandle) die ($error);
+  if($_GET["button"] == "newGame"){
+    $query = "SELECT rack, words FROM racks WHERE length=7 and weight <= 10 order by random() limit 0, 1";
+    $statement = $dbhandle->prepare($query);
+    $statement->execute();
+    $results = $statement->fetch(PDO::FETCH_ASSOC);
+    echo json_encode($results);
+  }
+  else if($_GET["button"] == "check"){
+    $query = "select * from racks where rack = '" . $_GET["rack"]."'";
+    $statement = $dbhandle->prepare($query);
+    $statement->execute();
+    $row = $statement->fetch(PDO::FETCH_ASSOC);
+    $word_arr = explode("@@", $row["words"]);
+    $incorrect = array("result" => "incorrect");
+    $correct = array("result" => "correct" , "guess"=>$_GET["guess"]);
+    if(in_array($_GET["guess"], $word_arr)){
+      echo json_encode($correct);
+    } 
+    else{
+      echo json_encode($incorrect);
+     }
+  }
 ?>
