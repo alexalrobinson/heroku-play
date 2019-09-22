d2243d16435aafe9fb68d248532b32d062a4a411
alexalrobinson <31484232+alexalrobinson@users.noreply.github.com>
2019-09-22 11:10:36 -0400

    Update api.php

--- api.php
+++ api.php
@@ -4,12 +4,12 @@
     $dbhandle = new PDO("sqlite:scrabble.sqlite") or die("Failed to open DB");
     if (!$dbhandle) die ($error);
 
-    if($_GET["button"] == "newRack"){
+    if($_GET["button"] == "newGame"){
     //this is a sample query which gets some data, the order by part shuffles the results
     //the limit 0, 10 takes the first 10 results.
     // you might want to consider taking more results, implementing "pagination", 
     // ordering by rank, etc.
-      $query = "SELECT rack, words FROM racks WHERE length>7 order by random() limit 1";
+      $query = "SELECT rack FROM racks WHERE length>7 order by random() limit 1";
     
     //this next line could actually be used to provide user_given input to theo 
     //avoid SQL injection attacks
@@ -22,16 +22,13 @@
     //this will naturally create a pleasant array of JSON data when I echo in a couple lines
 
       $results = $statement->fetchALL(PDO::FETCH_ASSOC);
-    
-    //this part is perhaps overkill but I wanted to set the HTTP headers and status code
-    //making to this line means everything was great with this request
-      header('HTTP/1.1 200 OK');
-    //this lets the browser know to expect json
-      header('Content-Type: application/json');
-    //this creates json and gives it back to the browser
       echo json_encode($results);
     }
     else {
-    
+        $query = "SELECT words FROM racks WHERE words LIKE $_GET[guess]";
+        $statement = $dbhandle->prepare($query);
+        $statement->execute();
+        $word = $statement->fetch(PDO::FETCH_ASSOC);
+        echo json_encode($word);
     }
 ?>
