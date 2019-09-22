0799f3528e1a095ebf692090c7e79ebbbc4a8c05
alexalrobinson <31484232+alexalrobinson@users.noreply.github.com>
2019-09-22 13:18:33 -0400

    Update api.php

--- api.php
+++ api.php
@@ -1,7 +1,7 @@
 <?php
 $dbhandle = new PDO("sqlite:scrabble.sqlite") or die("Failed to open DB");
     if (!$dbhandle) die ($error);
-	if($_GET["button"] == "newRack")
+	if($_GET["button"] == "newGame")
 	{
         $query = "SELECT rack, words FROM racks WHERE length=7 and weight <= 10 order by random() limit 0, 1";
     
