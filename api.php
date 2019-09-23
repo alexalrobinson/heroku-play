5c2cb9092e7b2f1ecb845af9540286f76c728eb4
alexalrobinson <31484232+alexalrobinson@users.noreply.github.com>
2019-09-23 11:41:33 -0400

    Update api.php

--- api.php
+++ api.php
@@ -8,19 +8,4 @@
     $results = $statement->fetch(PDO::FETCH_ASSOC);
     echo json_encode($results);
   }
-  else if($_GET["button"] == "check"){
-    $query = "select * from racks where rack = '" . $_GET["twistedtext"]."'";
-    $statement = $dbhandle->prepare($query);
-    $statement->execute();
-    $row = $statement->fetch(PDO::FETCH_ASSOC);
-    $word_arr = explode("@@", $row["words"]);
-    $incorrect = array("result" => "incorrect");
-    $correct = array("result" => "correct" , "guess"=>$_GET["guess"]);
-    if(in_array($_GET["guess"], $word_arr)){
-      echo json_encode($correct);
-    } 
-    else{
-      echo json_encode($incorrect);
-     }
-  }
 ?>
